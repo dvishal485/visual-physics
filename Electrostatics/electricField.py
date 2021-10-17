4d776d5db05cfe8a4df21d4715b03d4251970348
dvishal485 <dvishal485@gmail.com>
2021-10-17 15:51:02 +0530

    organized functions & added case of ring & work done function

--- Electrostatics/electricField.py
+++ Electrostatics/electricField.py
@@ -1,9 +1,11 @@
-from math import pi , sin
+from math import pi, sin
 from numpy import Infinity
 import calculationConstants
 import scipy.integrate as integrate
+import scipy.misc as diff
+import Electrostatics.electricPotential as potential
 
-k = pow((4*pi*calculationConstants.vaccumPermittivity), -1)
+k = 1/(4*pi*calculationConstants.vaccumPermittivity)
 
 
 def dueToPointCharge(charge, distance):
@@ -13,45 +15,61 @@ def dueToPointCharge(charge, distance):
         return (k * charge)/(pow(distance, 2))
 
 
-def dueToNonConductingSphere(chargeDensityFunction, radiusOfSphere, distanceFromCenter):
-    if min(radiusOfSphere, distanceFromCenter) == 0:
-        electricField = 0
-    else:
-        if distanceFromCenter > radiusOfSphere:
-            def integrand(x):
-                return chargeDensityFunction(x)*4*pi*(x**2)
-            totalEnclosedCharge = integrate.quad(
-                integrand, 0, radiusOfSphere)[0]
-            electricField = k*totalEnclosedCharge/(distanceFromCenter**2)
+class Sphere:
+    def dueToNonConductingSphere(chargeDensityFunction, radiusOfSphere, distanceFromCenter):
+        if min(radiusOfSphere, distanceFromCenter) == 0:
+            electricField = 0
         else:
-            def integrand(x):
-                return k * chargeDensityFunction(x)*4*pi*(x**2)/(distanceFromCenter**2)
-            electricField = integrate.quad(integrand, 0, distanceFromCenter)[0]
-    return electricField
+            if distanceFromCenter > radiusOfSphere:
+                def integrand(x):
+                    return chargeDensityFunction(x)*4*pi*(x**2)
+                totalEnclosedCharge = integrate.quad(
+                    integrand, 0, radiusOfSphere)[0]
+                electricField = k*totalEnclosedCharge/(distanceFromCenter**2)
+            else:
+                def integrand(x):
+                    return k * chargeDensityFunction(x)*4*pi*(x**2)/(distanceFromCenter**2)
+                electricField = integrate.quad(
+                    integrand, 0, distanceFromCenter)[0]
+        return electricField
+
+    def dueToUniformNonConductingSphere(linearChargeDensity, radiusOfSphere, distanceFromCenter):
+        def chargeDensityFunction(x):
+            return linearChargeDensity
+        return Sphere.dueToNonConductingSphere(chargeDensityFunction, radiusOfSphere, distanceFromCenter)
+
+    def dueToConductingSphere(surfaceChargeDensity, radiusOfSphere, distanceFromCenter):
+        if distanceFromCenter < radiusOfSphere:
+            electricField = 0
+        else:
+            chargeEnclosed = surfaceChargeDensity * \
+                (4 * pi * (radiusOfSphere**2))
+            electricField = k * chargeEnclosed / (distanceFromCenter**2)
+        return electricField
 
 
-def dueToUniformNonConductingSphere(linearChargeDensity, radiusOfSphere, distanceFromCenter):
-    def chargeDensityFunction(x):
-        return linearChargeDensity
-    return dueToNonConductingSphere(chargeDensityFunction, radiusOfSphere, distanceFromCenter)
+class LineCharge:
+    def dueToUniformLineCharge(linearChargeDensity, angleSubtendedA, angleSubtentedB, perpendicularDistanceFromBody):
+        if perpendicularDistanceFromBody == 0:
+            raise Exception(
+                'Electric field is not defined for a point on the line charge itself')
+        return k * linearChargeDensity * (sin(angleSubtendedA)+sin(angleSubtentedB))/perpendicularDistanceFromBody
+
+    def dueToUniformInfiniteLineCharge(chargeDensity, perpendicularDistanceFromBody):
+        return LineCharge.dueToUniformLineCharge(chargeDensity, pi/2, pi/2, perpendicularDistanceFromBody)
 
 
-def dueToUniformLineCharge(linearChargeDensity, angleSubtendedA, angleSubtentedB, perpendicularDistanceFromBody):
-    if perpendicularDistanceFromBody == 0:
-        raise Exception(
-            'Electric field is not defined for a point on the line charge itself')
-    return k * linearChargeDensity * (sin(angleSubtendedA)+sin(angleSubtentedB))/perpendicularDistanceFromBody
+class Ring:
+    def dueToUniformlyChargedRingOnAxis(linearChargeDensity, radiusOfRing, distanceFromCenter):
+        # Using E(x) = -dV/dx (or one can directly use formula from books)
+
+        def f(x):
+            return -potential.Ring.dueToUniformlyChargedRingOnAxis(linearChargeDensity, radiusOfRing, x)
+        return diff.derivative(f, distanceFromCenter, dx=10**-5)
 
 
-def dueToUniformInfiniteLineCharge(chargeDensity, perpendicularDistanceFromBody):
-    return dueToUniformLineCharge(chargeDensity, pi/2, pi/2, perpendicularDistanceFromBody)
-
-
-def dueToConductingSphere(surfaceChargeDensity, radiusOfSphere, distanceFromCenter):
-    if distanceFromCenter < radiusOfSphere:
-        electricField = 0
-    else:
-        chargeEnclosed = surfaceChargeDensity * \
-            (4 * pi * (radiusOfSphere**2))
-        electricField = k * chargeEnclosed / (distanceFromCenter**2)
-    return electricField
+class Disk:
+    def dueToUniformlyChargedDiskOnAxis(chargeDensity, radiusOfDisk, distanceFromCenter):
+        def f(x):
+            return -potential.Disk.dueToUniformlyChargedDiskOnAxis(chargeDensity, radiusOfDisk, x)
+        return diff.derivative(f, distanceFromCenter, dx=10**-5)
--- Electrostatics/electricPotential.py
+++ Electrostatics/electricPotential.py
@@ -1,69 +1,109 @@
 from math import pi
-from numpy import Infinity
+from numpy import Infinity, sign
 import calculationConstants
 import Electrostatics.electricField as Ef
 import scipy.integrate as integrate
 
-k = pow((4*pi*calculationConstants.vaccumPermittivity), -1)
+k = 1/(4*pi*calculationConstants.vaccumPermittivity)
 
 
 def dueToPointCharge(charge, distance):
     if distance == 0:
-        return Infinity
+        print('WARNING : Electric field/potential on the charge itself will be undefined')
+        return sign(charge)*Infinity
     else:
         return (k * charge)/distance
 
 # dV = -E.dr (when E vector exist only in direction of dr vector)
 
 
-def dueToNonConductingSphere(chargeDensityFunction, radiusOfSphere, distanceFromCenter):
-    def integrand(x):
-        return chargeDensityFunction(x)*4*pi*(x**2)
-    totalEnclosedCharge = integrate.quad(
-        integrand, 0, radiusOfSphere)[0]
-    if distanceFromCenter >= radiusOfSphere:
-        # for point outside or on the sphere
-        return k * totalEnclosedCharge / distanceFromCenter
-    else:
-        potentialAtSurface = k * totalEnclosedCharge / radiusOfSphere
-
+class Sphere:
+    def dueToNonConductingSphere(chargeDensityFunction, radiusOfSphere, distanceFromCenter):
         def integrand(x):
-            return Ef.dueToNonConductingSphere(
-                chargeDensityFunction, radiusOfSphere, x)
+            return chargeDensityFunction(x)*4*pi*(x**2)
+        totalEnclosedCharge = integrate.quad(
+            integrand, 0, radiusOfSphere)[0]
+        if distanceFromCenter >= radiusOfSphere:
+            # for point outside or on the sphere
+            return k * totalEnclosedCharge / distanceFromCenter
+        else:
+            potentialAtSurface = k * totalEnclosedCharge / radiusOfSphere
+
+            def integrand(x):
+                return Ef.dueToNonConductingSphere(
+                    chargeDensityFunction, radiusOfSphere, x)
+            excessPotential = integrate.quad(
+                integrand, distanceFromCenter, radiusOfSphere)[0]
+            # Potential at point inside sphere - Potential at surface = - integral(E.dx) from x to R
+            potentialAtPointInside = potentialAtSurface + excessPotential
+            return potentialAtPointInside
+
+    def dueToConductingSphere(surfaceChargeDensity, radiusOfSphere, distanceFromCenter):
+        totalChargeEnclosed = surfaceChargeDensity * \
+            4 * pi * (radiusOfSphere**2)
+        if distanceFromCenter > radiusOfSphere:
+            potential = k * totalChargeEnclosed / distanceFromCenter
+        else:
+            # E = 0 so V=constant=V surface
+            potential = k * totalChargeEnclosed / radiusOfSphere
+        return potential
+
+    def dueToUniformNonConductingSphere(linearChargeDensity, radiusOfSphere, distanceFromCenter):
+        def chargeDensityFunction(x):
+            return linearChargeDensity
+        return Sphere.dueToNonConductingSphere(chargeDensityFunction, radiusOfSphere, distanceFromCenter)
+
+
+class LineCharge:
+    def dueToSymmetericLineCharge(linearChargeDensity, angleSubtended, perpendicularDistanceFromBody):
+        # IT IS NOT AT ALL AN ABSOLUTE QUANTITY
+        # when angleSubtendedA = angleSubtendedB
+        # limitation as Electric field won't be in direction
+        # perpendicular to the charged body in other case
+        # or E vector doesn't align with dr vector
+        def integrand(x):
+            return Ef.dueToUniformLineCharge(
+                linearChargeDensity, angleSubtended, angleSubtended, x)
+        referencePotential = 0
+        # assuming potential at x=Infinity to be zero (which maybe treated as wrong)
         excessPotential = integrate.quad(
-            integrand, distanceFromCenter, radiusOfSphere)[0]
-        # Potential at point inside sphere - Potential at surface = - integral(E.dx) from x to R
-        potentialAtPointInside = potentialAtSurface + excessPotential
-        return potentialAtPointInside
+            integrand, perpendicularDistanceFromBody, Infinity)[0]
+        return referencePotential + excessPotential
 
 
-def dueToUniformNonConductingSphere(linearChargeDensity, radiusOfSphere, distanceFromCenter):
-    def chargeDensityFunction(x):
-        return linearChargeDensity
-    return dueToNonConductingSphere(chargeDensityFunction, radiusOfSphere, distanceFromCenter)
+class Ring:
+    def dueToChargedRingOnAxis(chargeOnRing, radiusOfRing, distanceFromCenter):
+        # All points are at a same distance from the ring's axis
+
+        potential = k * chargeOnRing / \
+            ((distanceFromCenter**2 + radiusOfRing**2)**(1/2))
+        return potential
+
+    def dueToUniformlyChargedRingOnAxis(linearChargeDensity, radiusOfRing, distanceFromCenter):
+        # Equivalent to Electric field due to Charged Ring on it's Axis
+        totalCharge = linearChargeDensity * (2*pi*radiusOfRing)
+        return Ring.dueToChargedRingOnAxis(totalCharge, radiusOfRing, distanceFromCenter)
 
 
-def dueToSymmetericLineCharge(linearChargeDensity, angleSubtended, perpendicularDistanceFromBody):
-    # IT IS NOT AT ALL AN ABSOLUTE QUANTITY
-    # when angleSubtendedA = angleSubtendedB
-    # limitation as Electric field won't be in direction
-    # perpendicular to the charged body in other case
-    # or E vector doesn't align with dr vector
-    def integrand(x):
-        return Ef.dueToUniformLineCharge(
-            linearChargeDensity, angleSubtended, angleSubtended, x)
-    referencePotential = 0
-    # assuming potential at x=Infinity to be zero (which maybe treated as wrong)
-    excessPotential = integrate.quad(
-        integrand, perpendicularDistanceFromBody, Infinity)[0]
-    return referencePotential + excessPotential
+class Disk:
+    def dueToChargedDiskOnAxis(chargeDensityFuntion, radiusOfDisk, distanceFromCenter):
+        def integrand(x):
+            return k * chargeDensityFuntion(x) * 2 * pi * x / (distanceFromCenter**2+x**2)**(1/2)
+        potential = integrate.quad(integrand, 0, radiusOfDisk)[0]
+        return potential
+
+    def dueToUniformlyChargedDiskOnAxis(chargeDensity, radiusOfDisk, distanceFromCenter):
+        def chargeDensityFuntion(chargeDensity):
+            return chargeDensity
+        return Disk.dueToChargedDiskOnAxis(chargeDensityFuntion, radiusOfDisk, distanceFromCenter)
 
 
-def dueToConductingSphere(surfaceChargeDensity, radiusOfSphere, distanceFromCenter):
-    totalChargeEnclosed = surfaceChargeDensity * 4 * pi * (radiusOfSphere**2)
-    if distanceFromCenter > radiusOfSphere:
-        potential = k * totalChargeEnclosed / distanceFromCenter
-    else:
-        # E = 0 so V=constant=V surface
-        potential = k * totalChargeEnclosed / radiusOfSphere
-    return potential
+class Work:
+    def workDoneByUs(finalPotential, initialPotential, chargeTransferred):
+        return chargeTransferred * (finalPotential-initialPotential)
+
+    def changeInPotentialEnergy(finalPotential, initialPotential, chargeTransferred):
+        return chargeTransferred * (finalPotential-initialPotential)
+
+    def workDoneByElectricForce(finalPotential, initialPotential, chargeTransferred):
+        return -chargeTransferred * (finalPotential-initialPotential)
